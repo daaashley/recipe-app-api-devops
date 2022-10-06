cd79b96b81f422a0fef7d1feaf1a1604690d67b6
David Ashley <david.ashley@kochgs.com>
2022-10-05 21:35:31 -0500

    new tags

--- deploy/main.tf
+++ deploy/main.tf
@@ -15,5 +15,11 @@ provider "aws" {
 
 locals {
   prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManagedBy   = "Terraform"
+  }
 }
 
